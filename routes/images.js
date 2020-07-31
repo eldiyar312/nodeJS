48d81b0f52fda992a267cca38f3d0573b7948c3f
eldiyar312 <eldiyar.san@gmail.com>
2020-08-01 03:45:05 +0600

    very very last push

--- routes/images.js
+++ routes/images.js
@@ -1,13 +1,21 @@
 const {Router} = require('express')
 const Image = require('../model/Image')
+const cors = require('cors')
 
 const router = Router()
 
+// Cors
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+
 /* Image */
-router.get('/images', async (req, res) => {
+router.get('/images', cors(corsOptions), async (req, res) => {
   try {
     Image.find()
-      .then(images => res.status(201).json({images}))
+      .then(images => res.status(200).json({images}))
   } catch (e) {
     res.status(400).json({message: 'error post'})
     res.end()
