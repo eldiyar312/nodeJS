8b4d59840c656ee59e4f3cb437c6e3c38f4e9eef
eldiyar312 <eldiyar.san@gmail.com>
2020-08-29 04:20:50 +0600

    clear code

--- routes/images.js
+++ routes/images.js
@@ -1,25 +1,23 @@
 const { Router } = require('express')
 const Image = require('../model/Image')
 const cors = require('cors')
+const config = require('config')
 
 const router = Router()
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+
 
 /* Open API */
 router.get(
   '/images', 
-  cors( corsOptions ), 
+  cors( config.get('corsOptions') ), 
   ( req, res ) => {
     try {
 
       Image.find()
-        .then( images => res.status(200).json({ images }) )
+        .then( images => res.status( 200 ).json({ images }) )
 
     } catch (e) {
-      res.status(400).json({message: e})
+      res.status(400).json({ message: e })
       res.end()
     }
 })
--- routes/imgFile.js
+++ routes/imgFile.js
@@ -1,34 +1,33 @@
 const { Router } = require('express')
 const path = require('path')
+const config = require('config')
 const cors = require('cors')
 
 
 const router = Router()
 
-// CORS
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+router.get(
+  '/img/:name',
+  cors(config.get('corsOptions')),
+  (req, res, next) => {
 
-router.get('/img/:name', cors(corsOptions), (req, res, next) => {
-  const options = {
-    root: path.join('img'),
-    dotfiles: 'deny',
-    headers: {
-      'x-timestamp': Date.now(),
-      'x-sent': true
+    const options = {
+      root: path.join('img'),
+      dotfiles: 'deny',
+      headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+      }
     }
-  }
 
-  const fileName = req.params.name
-  res.sendFile(fileName, options, function (err) {
-    if (err) {
-      next(err)
-    } else {
-      console.log('Sent:', fileName)
-    }
+    const fileName = req.params.name
+    res.sendFile(fileName, options, err => {
+      if (err) {
+        next(err)
+        res.status( 400 ).json({ message: err })
+      }
+    })
+
   })
-})
 
 module.exports = router
